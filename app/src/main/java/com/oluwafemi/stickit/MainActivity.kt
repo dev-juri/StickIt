0e62a5e823481442ae2bdcf354598f15c97c5c88
dev-juri <ojurifemi132@gmail.com>
2020-07-19 12:40:06 +0100

    Setup UI

--- app/src/main/java/com/oluwafemi/stickit/MainActivity.kt
+++ app/src/main/java/com/oluwafemi/stickit/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.oluwafemi.stickit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.oluwafemi.stickit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //Creates a shared preference file
+        val sharedPref = getSharedPreferences(getString(R.string.file_key), Context.MODE_PRIVATE) ?: return
+
+        val presentNote = sharedPref.getString(getString(R.string.our_note), "")
+         //Check if there's anything in the shared preference
+        if(presentNote == null){
+            //Create a value for the note
+            with(sharedPref.edit()) {
+                putString(getString(R.string.our_note), "")
+                apply()
+            }
+        }
+
+        binding.stickyNote.setText(presentNote)
+
     }
 }
---
+++ app/src/main/java/com/oluwafemi/stickit/StickyNoteActivity.kt
@@ -0,0 +1,39 @@
+package com.oluwafemi.stickit
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+
+class StickyNoteActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val sharedPref = getSharedPreferences(getString(R.string.file_key), Context.MODE_PRIVATE) ?: return
+
+        val presentNote = sharedPref.getString(getString(R.string.our_note), "")
+
+        //Check for the intent filter action
+        if (intent.action == Intent.ACTION_PROCESS_TEXT){
+
+            //Get the text processed
+            val interStick = intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT) ?: ""
+
+            if(presentNote.isNullOrEmpty()){
+                with(sharedPref.edit()){
+                    putString(getString(R.string.our_note), interStick)
+                }
+            } else {
+                with(sharedPref.edit()){
+                    putString(getString(R.string.our_note), "$presentNote\n\n$interStick")
+                }
+            }
+            Toast.makeText(this, "Clipped: $interStick", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
